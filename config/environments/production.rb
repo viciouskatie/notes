603f9110ac0d1a33ecb0cd264f488e8661966bdd
viciouskatie <kikat31@yahoo.com>
2021-02-18 11:29:08 -0600

    heroku tech

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,3 +118,8 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+begin
+  require ‘minitest/autorun’
+rescue LoadError => e
+  raise e unless ENV[‘RAILS_ENV’] == “production”
+end
