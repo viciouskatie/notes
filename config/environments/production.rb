a01e0100b02046de8a2bdcba0e94216bb0e1b9ac
viciouskatie <kikat31@yahoo.com>
2021-02-18 14:31:48 -0600

    rmv minitests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,8 +118,3 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
-begin
-  require ‘minitest/autorun’
-rescue LoadError => e
-  raise e unless ENV[‘RAILS_ENV’] == “production”
-end
